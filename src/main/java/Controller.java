893c13cb1597727b10a8a42f80aa4df5399e6919
Rae Do <raellysillied@gmail.com>
2025-12-01 16:07:31 +0000

    added error messaging

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -10,15 +10,17 @@ import java.util.ArrayList;
 public class Controller {
     public String correctWord;
     private int currentLetter = 1; // column + 1
-    private int currentAttempt = 1; // row + 1
+    public int currentAttempt = 1; // row + 1
     private int maxAttempts;
     private String currentGuess = "";
     private Rectangle[][] tiles; // copy reference/"pointer" (heap) (row, column)
     private Text[][] characters; // same as above
-    private ArrayList<String> greenChars = new ArrayList<String>();
+    private ArrayList<String> greenButtons = new ArrayList<String>();
+    private ArrayList<String> yellowButtons = new ArrayList<String>();
     private Button[] topKeyboardStyle = new Button[10];  // Q-P
     private Button[] middleKeyboardStyle = new Button[9]; // A-L
     private Button[] bottomKeyboardStyle = new Button[7]; // Z-M
+    private boolean wordCorrect = false;
 
     public Controller(String correctWord, Rectangle[][] tiles, Text[][] characters, int maxAttempts) { // update these spare copies
         this.correctWord = correctWord.toLowerCase();
@@ -28,43 +30,56 @@ public class Controller {
     }
 
     public void addCharacter (char letter) {
-        if (currentLetter <= 5 && currentAttempt <= maxAttempts) {
+        if (currentLetter <= 5 && (currentAttempt <= maxAttempts && !wordCorrect)) {
             characters[currentAttempt - 1][currentLetter - 1].setText(String.valueOf(letter).toUpperCase());
             currentGuess += Character.toLowerCase(letter);
             currentLetter++;
             System.out.println(currentGuess);
+        } else {
+            if (!wordCorrect) {
+                System.out.println("you already got the word brochacho");
+                return;
+            }
+            System.out.println("you out of space lmao");
         }
     }
 
     public void backspace() {
-        if (currentLetter > 1 && currentAttempt <= maxAttempts) {
+        if (currentLetter > 1 && (currentAttempt <= maxAttempts && !wordCorrect)) {
             currentLetter--;
             characters[currentAttempt - 1][currentLetter - 1].setText("");
             if (!currentGuess.isEmpty()) {
                 currentGuess = currentGuess.substring(0, currentGuess.length() - 1);
+            } else {
+                System.out.println("this shouldn't ever happen (loop logic) but nothing to backspace");
             }
         }
     }
 
-    public void submitGuess() {
+    public String submitGuess() { // IMPORTANT: this is like the bulk of the logic LOL
         if (currentAttempt > maxAttempts) {
             System.out.println("ur out of guesses lmao");
-            return;
+            return "Out of guesses";
+        }
+
+        if (wordCorrect) {
+            System.out.println("you already got the word brochacho");
+            return "Word has been gotten";
         }
 
         if (currentGuess.length() != correctWord.length()) { // check if full word is given
             System.out.println("There are empty slots.");
-            return;
+            return "Not enough letters";
         }
 
         WordBank checkGuess = new WordBank(); // check if word is valid in word bank
         if (!checkGuess.checkWord(currentGuess)) {
             // System.out.println("Word not in bank.");
-            return;
+            return "Not in word list";
         }
 
-        char[] guessArray = currentGuess.toCharArray();
-        char[] correctWordArray = correctWord.toCharArray();
+        char[] guessArray = currentGuess.toCharArray(); // array of the guess (each index is a character)
+        char[] correctWordArray = correctWord.toCharArray(); // same thing for the correct word
         int[] letterInc = new int [26]; // (0 <-> A, 1 <-> B, etc, dictates number of a character in the correct word)
         for (char x : correctWordArray) { // inc each alphabetical numerical slot by 1 for each time it shows up in the answer
             letterInc[x - 'a']++; // each alphabetical character increase also increases its unicode index by one, get "distance" from 'a'
@@ -72,16 +87,16 @@ public class Controller {
         }
 
         for (int i = 0; i < currentGuess.length(); i++) { // first pass for greens
-            char guessedChar = currentGuess.charAt(i);
+            char guessedChar = currentGuess.charAt(i); // correct character at the index
             Rectangle tile = tiles[currentAttempt - 1][i];
             Text character = characters[currentAttempt - 1][i];
 
             if (correctWord.charAt(i) == guessedChar) {
-                tile.setFill(Variables.WORDLE_GREEN); // GREEN
+                tile.setFill(Variables.WORDLE_GREEN);
                 tile.setStroke(Variables.WORDLE_GREEN);
                 character.setFill(Color.WHITE);
-                letterInc[guessedChar - 'a']--;
-                greenChars.add(Character.toString(guessedChar));
+                letterInc[guessedChar - 'a']--; // decrease letter inc if guessed properly (how many chars not gotten)
+                greenButtons.add(Character.toString(guessedChar)); // add to arraylist for letters that have been greenlit
                 guessArray[i] = '*'; // mark character index on the guess array that is registered
                 // (W|A|T|E|R -> W|*|T|E|R)
                 updateKeyColor(correctWord.charAt(i), "green");
@@ -97,13 +112,13 @@ public class Controller {
                 continue; // skip, already marked as green
             }
 
-            if (letterInc[guessedChar - 'a'] > 0) {
+            if (letterInc[guessedChar - 'a'] > 0) { // this checks if our letter inc still has any remaining letters not guessed
                 tile.setFill(Variables.WORDLE_YELLOW);
                 tile.setStroke(Variables.WORDLE_YELLOW);
                 character.setFill(Color.WHITE);
                 letterInc[guessedChar - 'a']--;
                 boolean isGreen = false;
-                for (String x : greenChars) {
+                for (String x : greenButtons) { // this code checks our array of greenlit chars, making sure it doesnt change back
                     char green = Character.toLowerCase(x.charAt(0));
                     if (green == guessedChar) {
                         isGreen = true;
@@ -112,18 +127,35 @@ public class Controller {
                 }
                 if (!isGreen) {
                     updateKeyColor(currentGuess.charAt(i), "yellow");
+                    yellowButtons.add(Character.toString(currentGuess.charAt(i)));
                 }
             } else {
                 tile.setFill(Variables.WORDLE_GRAY);
                 tile.setStroke(Variables.WORDLE_GRAY);
                 character.setFill(Color.WHITE);
-                updateKeyColor(currentGuess.charAt(i), "gray");
+                boolean isYellow = false;
+                for (String x : yellowButtons) { // same concept as before for checking for buttons already green
+                    char yellow = Character.toLowerCase(x.charAt(0));
+                    if (yellow == guessedChar) {
+                        isYellow = true;
+                        break;
+                    }
+                }
+                if (!isYellow) {
+                    updateKeyColor(currentGuess.charAt(i), "gray");
+                }
             }
         }
-
         currentAttempt++;
         currentLetter = 1;
+        if (currentGuess.toLowerCase().equals(correctWord.toLowerCase())) {
+            wordCorrect = true;
+            currentGuess = "";
+            return "WIN";
+
+        }
         currentGuess = "";
+        return "No error";
     }
 
     public void addKeys(Button b, int index, String keyboardRow) { // import keyboard rows
@@ -177,4 +209,5 @@ public class Controller {
         }
         return null;
     }
+
 }
--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -18,7 +18,9 @@ import javafx.scene.layout.StackPane;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import javafx.animation.FadeTransition;
+import javafx.util.Duration;
+import javafx.scene.control.Label;
 
 public class UI extends Application {
 
@@ -27,21 +29,24 @@ public class UI extends Application {
     mix tbh) is just really messy
      */
 
-    public static int rows = 6; // guesses allowed
+    public static int rows = 6; // guesses allowed (base 6)
     private static int cols = 5; // word length lol
     private Rectangle[][] tiles = new Rectangle[rows][cols];
     private Text[][] characters = new Text[rows][cols];
     private static String correctWord;
     private Controller controller = new Controller("", tiles, characters, rows);
+    private StackPane guessingScreen = new StackPane();
+    private boolean freezeGuessingInput = false;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Wordel");
 
+
         // Wordle tiles
         GridPane wordGrid = new GridPane();
         wordGrid.setAlignment(Pos.TOP_CENTER);
-        wordGrid.setTranslateY(-30);
+        wordGrid.setTranslateY(20);
         wordGrid.setHgap(5);
         wordGrid.setVgap(5);
         for (int row = 0; row < rows; row++) { // scan each row (up to down)
@@ -77,6 +82,9 @@ public class UI extends Application {
             // button.setText(buttonText);
             button.setFocusTraversable(false);
             button.setOnAction(e -> {
+                if (freezeGuessingInput) {
+                    return;
+                }
                 if (controller != null) {
                     controller.addCharacter(Character.toLowerCase(topKeyboardCharacter));
                 }
@@ -100,6 +108,9 @@ public class UI extends Application {
             // button.setText(buttonText);
             button.setFocusTraversable(false); // prevent taking in enter or space key as pressing the button (Q is first letter)
             button.setOnAction(e -> {
+                if (freezeGuessingInput) {
+                    return;
+                }
                 if (controller != null) {
                     controller.addCharacter(Character.toLowerCase(middleKeyboardCharacter));
                 }
@@ -119,8 +130,20 @@ public class UI extends Application {
         String enterText = "ENTER";
         enter.setFocusTraversable(false);
         enter.setOnAction(e -> {
+            if (freezeGuessingInput) {
+                return;
+            }
             if (controller != null) {
-                controller.submitGuess();
+                System.out.println("onscreen keyboard");
+                String errorMessage = controller.submitGuess();
+                if (errorMessage.equals("No error")) {
+                    return;
+                }
+                if (errorMessage.equals("WIN")) {
+                    showWinScreen();
+                    return;
+                }
+                showMessage(errorMessage, guessingScreen, 0.4);
             }
         });
         bottomKeyboardRow.getChildren().add(enter);
@@ -134,6 +157,9 @@ public class UI extends Application {
             // button.setText(buttonText);
             button.setFocusTraversable(false);
             button.setOnAction(e -> {
+                if (freezeGuessingInput) {
+                    return;
+                }
                 if (controller != null) {
                     controller.addCharacter(Character.toLowerCase(bottomKeyboardCharacter));
                 }
@@ -150,6 +176,9 @@ public class UI extends Application {
         String backspacetext = "BACKSPACE";
         backspace.setFocusTraversable(false);
         backspace.setOnAction(e -> {
+            if (freezeGuessingInput) {
+                return;
+            }
             if (controller != null) {
                 controller.backspace();
             }
@@ -158,11 +187,12 @@ public class UI extends Application {
 
         VBox keyboard = new VBox(10, topKeyboardRow, middleKeyboardRow, bottomKeyboardRow); // complete keyboard
         keyboard.setAlignment(Pos.CENTER);
-        keyboard.setTranslateY(-40);
+        keyboard.setTranslateY(10);
 
         // Main layout of children of the guessing root
         VBox rootGuessing = new VBox(20, wordGrid, keyboard);
-        rootGuessing.setAlignment(Pos.CENTER);
+        rootGuessing.setAlignment(Pos.TOP_CENTER);
+        guessingScreen.getChildren().add(rootGuessing);
         primaryStage.setResizable(false);
 
         // Choosing screen
@@ -205,29 +235,40 @@ public class UI extends Application {
 
         // Setup to take in physical keyboard inputs
         scene.setOnKeyPressed(e -> {
+            if (freezeGuessingInput) {
+                return;
+            }
         KeyCode code = e.getCode();
         if (controller == null) {
             return;
         }
-        switch (code) {case BACK_SPACE:
-                controller.backspace();
-                break;
+            switch (code) {
+                case BACK_SPACE:
+                    controller.backspace();
+                    break;
 
-            case ENTER:
-                controller.submitGuess();
-                break;
+                case ENTER:
+                case SPACE:
+                    if (controller != null) {
+                        String errorMessage = controller.submitGuess();
+                        if (errorMessage.equals("No error")) {
+                            return;
+                        }
+                        if (errorMessage.equals("WIN")) {
+                            showWinScreen();
+                            return;
+                        }
+                        showMessage(errorMessage, guessingScreen, 0.4);
+                        break;
+                    }
 
-            case SPACE:
-                controller.submitGuess();
-                break;
-
-            default:
-                if (code.isLetterKey()) {
-                    controller.addCharacter(code.getName().toLowerCase().charAt(0));
-                }
-                break;
-        }
-    });
+                default:
+                    if (code.isLetterKey()) {
+                        controller.addCharacter(code.getName().toLowerCase().charAt(0));
+                    }
+                    break;
+            }
+        });
 
         // Okay, this changes the root if proper word is given on the choosing screen
         chosenWord.setOnAction(e -> {
@@ -241,10 +282,11 @@ public class UI extends Application {
                 correctWord = chosenWord.getText().toLowerCase();
                 System.out.println(correctWord);
                 controller.correctWord = chosenWord.getText().toLowerCase(); // controlaa
-                scene.setRoot(rootGuessing);
+                scene.setRoot(guessingScreen);
                 chosenWord.setDisable(true); // disable choosing screen nodes, TODO: fix when implementing a gameplay loop
                 submit.setDisable(true);
                 random.setDisable(true);
+                guessingScreen.setDisable(false);
             }
         });
 
@@ -261,6 +303,42 @@ public class UI extends Application {
         // showtime
         primaryStage.show();
     }
+
+    private void showMessage(String message, StackPane screen, double seconds) {
+        Label userFB = new Label(message);
+        userFB.setFont(Font.font("Arial", FontPosture.ITALIC, 16));
+        userFB.setStyle("-fx-background-color: black; -fx-background-radius: 6px; -fx-text-fill: white; -fx-padding: 10px 16px;");
+        userFB.setOpacity(0);
+        userFB.setAlignment(Pos.CENTER);
+        userFB.setTranslateY(-330);
+        userFB.setPrefHeight(40);
+        screen.getChildren().add(userFB);
+
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(20), userFB);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+
+        FadeTransition fadeOut = new FadeTransition(Duration.millis(500), userFB);
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+        fadeOut.setDelay(Duration.seconds(seconds));
+
+        fadeIn.setOnFinished(e -> fadeOut.play());
+        fadeOut.setOnFinished(e -> screen.getChildren().remove(screen));
+
+        fadeIn.play();
+    }
+
+    public void showWinScreen() {
+        freezeGuessingInput = true;
+        String[] winMessages = {"Genius", "Magnificent", "Impressive", "Splendid", "Phew"};
+        if (rows - (controller.currentAttempt) == 0) {
+            showMessage(winMessages[4], guessingScreen, 4);
+        } else {
+            showMessage(winMessages[(controller.currentAttempt - 1)], guessingScreen, 4);
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
